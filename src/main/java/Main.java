aeece5394f4002fbd213d11a3b64fa3a4e6d452a
murilo-l1 <murilolusvarghi@alunos.utfpr.edu.br>
2024-07-11 16:13:43 -0300

    second stage first try

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,23 @@
 import java.util.Scanner;
 
 public class Main {
+
+    static String[] validCommands = {"cd", "echo"};
+    static String invalidCommandMessage = ": command not found";
+
     public static void main(String[] args) throws Exception {
 
         System.out.print("$ ");
 
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
+
+        for(String command : validCommands){
+            if(input.equals(command)){
+                System.out.println("Command found!");
+            }
+        }
+        System.out.printf("%s%s", input, invalidCommandMessage);
+
     }
 }
